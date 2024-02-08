a449984984d24cce611aa7d8350a8e25a5169a0b
DeltDev <deltenc88@gmail.com>
2024-02-08 18:20:26 +0700

    GameMatrix object adding instructions to functions

---
+++ gamematrix.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#include "token.h"
+#include "gamematrix.h"
+
+using namespace std;
+
+GameMatrix::GameMatrix(int Row, int Col){
+    RowSize = Row;
+    ColSize = Col;
+    isGameMatrixValid = false;
+    InitiateTokenMatrix();
+    InitiateVisitedMatrix();
+}
+void GameMatrix::InitiateTokenMatrix(){
+    for(int i = 0; i<RowSize; i++){
+        vector<Token> rowInit;
+        
+        for(int j = 0; j<ColSize; j++){
+            Token TokenInit = Token(".");
+            rowInit.push_back(TokenInit);
+        }
+        TokenMatrix.push_back(rowInit);
+    }
+} 
+void GameMatrix::InitiateVisitedMatrix(){
+    for(int i = 0; i<RowSize; i++){
+        vector<bool> rowInit;
+        for(int j = 0; j<ColSize; j++){
+            rowInit.push_back(false);
+        }
+        VisitedMatrix.push_back(rowInit);
+    }
+} 
+Token GameMatrix::GetTokenCell(int Row, int Col){
+    return TokenMatrix[Row][Col];
+}
+bool GameMatrix::GetVisitedCell(int Row, int Col){
+    return VisitedMatrix[Row][Col];
+}
+
+void GameMatrix::SetVisitedCell(int Row, int Col, bool cellValue){
+    VisitedMatrix[Row][Col] = cellValue;
+    return;
+}
+
+int GameMatrix::GetCol(){
+    return ColSize;
+}
+
+int GameMatrix::GetRow(){
+    return RowSize;
+}
+
+void GameMatrix::PrintTokenMatrix(){
+    for(int i = 0; i<RowSize; i++){
+        for(int j = 0; j<ColSize; j++){
+            if(j == ColSize-1){
+                cout<<TokenMatrix[i][j].GetTokenString();
+            } else{
+                cout<<TokenMatrix[i][j].GetTokenString() + " ";
+            }
+        }
+        cout<<"\n";
+    }
+}
+
+void GameMatrix::PrintVisitedMatrix(){
+    for(int i = 0; i<RowSize; i++){
+        for(int j = 0; j<ColSize; j++){
+            if(j == ColSize-1){
+                cout<<VisitedMatrix[i][j];
+            } else{
+                cout<<VisitedMatrix[i][j] + " ";
+            }
+        }
+        cout<<"\n";
+    }
+}
+bool GameMatrix::GetGameMatrixValidity(){
+    return isGameMatrixValid;
+}
--- gamematrix.h
+++ gamematrix.h
@@ -4,14 +4,22 @@ using namespace std;
 //GameMatrix adalah matriks yang berisi token dan matriks yang berisi boolean untuk menentukan apakah cell sudah dikunjungi
 class GameMatrix{
     private:
+        int ColSize,RowSize;
         vector<vector<Token> > TokenMatrix; //matriks yang hanya berisi token
         vector<vector<bool> > VisitedMatrix; //matriks yang menunjukkan apakah cell sudah dikunjungi atau belum
-        void InitiateTokenMatrix(int Row, int Col); //membuat matriks token baru
-        void InitiateVisitedMatrix(int Row, int Col); //membuat matriks visited baru
+        bool isGameMatrixValid;
+        void InitiateTokenMatrix(); //membuat matriks token baru
+        void InitiateVisitedMatrix(); //membuat matriks visited baru
+        void CheckGameMatrixValidity(); //mengecek apakah GameMatrix valid
     public:
         GameMatrix(int Row, int Col); //konstruktur GameMatrix
         Token GetTokenCell(int Row, int Col); //mendapatkan token pada baris dan kolom yang dicari
         bool GetVisitedCell(int Row, int Col); //mendapatkan nilai apakah cell sudah dikunjungi atau belum
         void InputGameMatrix(int Row, int Col); //menginput gameMatrix
         void SetVisitedCell(int Row, int Col, bool cellValue); //mengubah nilai cell visited matrix (untuk backtracking)
+        int GetRow(); //mendapatkan banyak baris matriks
+        int GetCol(); //mendapatkan banyak kolom matriks
+        void PrintTokenMatrix(); //mencetak matriks token
+        void PrintVisitedMatrix(); //mencetak matriks visited
+        bool GetGameMatrixValidity(); //mendapatkan nilai isGameMatrixValid
 };
