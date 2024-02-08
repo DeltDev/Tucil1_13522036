eec1c38cb09d034df23f6b4c9310bf1694cd3668
DeltDev <deltenc88@gmail.com>
2024-02-08 22:42:53 +0700

    String List To Token List Implementation

--- helper.cpp
+++ helper.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 #include "token.h"
-
+#include "helper.h"
 using namespace std;
 
+vector<string> StringToStringList(string inputString){
+
+}
+vector<Token> StringListToTokenList(vector<string> StringList, set<string> ValidTokens){
+    vector<Token> ListOut;
+    for(int i = 0; i<StringList.size(); i++){
+        Token temp = Token(StringList[i],ValidTokens);
+        ListOut.push_back(temp);
+    }
+    return ListOut;
+}
 void PrintTokenList(vector<Token> TokenList){
     for(int i = 0; i<TokenList.size(); i++){
         cout<<TokenList[i].GetTokenString()<<"\n";
--- helper.h
+++ helper.h
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 #include "token.h"
 using namespace std;
-vector<Token> StringToTokenList(string inputString); //untuk melakukan stripping token dari string yang mengandung spasi
-
+vector<string> StringToStringList(string inputString); //untuk melakukan stripping token dari string yang mengandung spasi
+vector<Token> StringListToTokenList(vector<string> StringList, set<string> ValidTokens);
 //debugger
 void PrintTokenList(vector<Token> TokenList);
 
