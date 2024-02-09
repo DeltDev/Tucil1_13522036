ada04b726b83a5243f74ec193e36cc8ad1727816
DeltDev <deltenc88@gmail.com>
2024-02-09 11:43:27 +0700

    Brute force solution WIP

--- main.cpp
+++ main.cpp
@@ -5,26 +5,70 @@
 #include "gamematrix.h"
 using namespace std;
 //Atribut untuk menyelesaikan tucil
-string OptimalAnswer; //string jawaban
-int pointsCounter; //poin yang dipegang sekarang
-int MaxPoints; //poin maksimal
-int bufferCounter; //banyak buffer yang diambil sekarang
-int MinBuffer; //buffer minimal
+//Atribut jawaban akhir
+string OptimalFinalAnswer; //string jawaban
+int MaxPoints; //jawaban poin maksimal
+int MinBuffer; //jawaban buffer minimal
+vector<Token> TokenListFinalAnswer; //daftar token yang dipilih sebagai jawaban akhir
+vector<pair<int,int> > CoordinateListFinalAnswer; //daftar koordinat yang dipilih sebagai jawaban akhir
 
 int MatrixRow,MatrixCol; //panjang baris dan kolom kedua matriks di atas
 int bufferSize; //panjang buffer
-int sequenceSize;
-stack<pair<int,int> > CoordinateStack;//stack yang berisi koordinat tempat
+int sequenceSize;//banyak sequence
+vector<pair<int,int> > CoordinateStack;//stack yang berisi koordinat tempat
 vector<TokenSequence> SequenceList; //daftar sequence yang ada
 vector<bool> SequenceCheckList; //list yang berfungsi untuk mengecek apakah sequence sudah pernah ada atau belum
 
-bool isTokenMatrixValid; //mengecek apakah semua token pada matriks valid
 bool AllSequenceValid; //mengecek apakah semua sequence valid
 set<string> validTokens;// himpunan daftar token yang valid
 vector<vector<string> > inputStringMatrix;
 //debugging
 // string test,out;
 // stack<string> Stack1, Stack2;
+void SolveOptimal(GameMatrix gameMatrix, string state, string currentAnswer, int BufferLeft, int currentPoints, 
+vector<Token> currentTokenList, vector<pair<int, int> >  currentCoordinateList,int currentRow, int currentCol ,
+int currentBuffer, vector<bool> currentSequenceCheckList, vector<TokenSequence> currentSequenceList){
+    if(BufferLeft<=0){ //Basecase: jika BufferLeft<=0, jangan jalankan prosedur ini karena buffer sudah penuh.
+        return;
+    }
+
+    if(state == "Horizontal"){
+        for(int i = 0; i<MatrixCol; i++){
+            if(gameMatrix.GetVisitedCell(currentRow,i)){//Jika token di sel[currentRow][i] belum dikunjungi
+                string newAnswer = currentAnswer + gameMatrix.GetTokenCell(currentRow,i).GetTokenString(); //Tambahkan string token yang ditunjuk ke string answer
+                vector<Token> newTokenList = currentTokenList; 
+                newTokenList.push_back(gameMatrix.GetTokenCell(currentRow,i));//Push token ke TokenList dari belakang
+                pair<int,int> currentCoordinate = make_pair(i+1,currentRow+1);
+                vector<pair<int,int> > newCoordinateList = currentCoordinateList;
+                newCoordinateList.push_back(currentCoordinate); //Push koordinat saat ini ke CoordinateList dari belakang
+                gameMatrix.SetVisitedCell(currentRow,i,true); //Tandai sel[currentRow][i] sudah dikunjungi
+                int newBufferLeft,newPoints,newBufferCount,PointAddition;
+                newBufferLeft = BufferLeft-1; //Kurangi BufferLeft dengan 1
+                newBufferCount = currentBuffer+1; //Tambahkan currentBuffer dengan 1
+                PointAddition = 0; //Buat variabel PointAddition untuk menentukan tambahan poin dengan cara berikut:
+                vector<bool> newSequenceCheckList = currentSequenceCheckList;
+                for(int i = 0; i<currentSequenceList.size(); i++){ //1.	Cek semua sequence di SequenceList
+                    if(!newSequenceCheckList[i]){ //Jika nilai SequenceCheckList dari Sequence yang diperiksa bernilai false
+                        if(newAnswer.find(currentSequenceList[i].GetSequenceString()) != string::npos){//Jika string dari sequence tersebut adalah substring dari newAnswer, maka
+                            PointAddition+=currentSequenceList[i].GetSequencePoints(); //Tambahkan PointAddition dengan poin yang dimiliki oleh sequence tersebut
+                            newSequenceCheckList[i] = true; //Tandai SequenceCheckList dari sequence yang diperiksa jadi true
+                        }
+                    }
+                }
+                newPoints = currentPoints + PointAddition; //Tambahkan currentPoints dengan PointAddition
+                //BELUM DITAMBAHKAN Update jawaban
+                SolveOptimal(gameMatrix,"Vertical",newAnswer,newBufferLeft,newPoints,newTokenList,newCoordinateList,currentRow,i,newBufferCount,newSequenceCheckList,currentSequenceList);
+                //Panggil fungsi ini lagi secara rekursif dengan parameter yang sudah diupdate di atas ditambah dengan nilai currentCol = i, nilai currentRow sama, dan state = “Vertical”
+                //BELUM DIKERJAKAN Batalkan semua perubahan parameter untuk iterasi berikutnya
+            }
+        }
+    } else if(state == "Vertical"){
+        //BELUM DIKERJAKAN
+    }
+}
+void UpdateAnswer(){
+
+}
 int main(){
     cout<<"Masukkan panjang buffer\n";
     cin>>bufferSize;
